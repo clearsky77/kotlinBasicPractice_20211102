7f778e4b3690424dd3d894d75c29c8824f3b9cad
clearsky77 <clearsky3264@gmail.com>
2021-11-02 16:02:12 +0900

    Initial commit

---
+++ app/src/main/java/com/clearsky77/kotlinbasicpractice_20211102/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.clearsky77.kotlinbasicpractice_20211102
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
